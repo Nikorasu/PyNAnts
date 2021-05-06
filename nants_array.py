7705ca3f6e0bbcca0bc9500d069467ec802473bb
Nikorasu <nikorasu85@gmail.com>
2021-05-06 14:04:12 -0500

    Ants now check pheroArray for colors, functionized sensor checks.

--- nants_array.py
+++ nants_array.py
@@ -8,7 +8,7 @@ NAnts
 Copyright (c) 2021  Nikolaus Stromberg  nikorasu85@gmail.com
 '''
 FLLSCRN = True          # True for Fullscreen, or False for Window.
-ANTS = 100              # Number of Ants to spawn.
+ANTS = 200              # Number of Ants to spawn.
 WIDTH = 1200            # default 1200
 HEIGHT = 800            # default 800
 FPS = 60                # 48-90
@@ -22,7 +22,7 @@ class Ant(pg.sprite.Sprite):
         self.nest = nest
         self.image = pg.Surface((12, 21)).convert()
         self.image.set_colorkey(0)
-        cBrown = (80,42,42)
+        cBrown = (100,42,42)
         # Draw Ant
         pg.draw.aaline(self.image, cBrown, [0, 5], [11, 15])
         pg.draw.aaline(self.image, cBrown, [0, 15], [11, 5]) # legs
@@ -45,56 +45,69 @@ class Ant(pg.sprite.Sprite):
     def update(self, dt):  # behavior
         curW, curH = self.drawSurf.get_size()
         mid_result = left_result = right_result = [0,0,0]
+        mid_A_result = left_A_result = right_A_result = [0,0,0]
         randAng = randint(0,360)
         accel = pg.Vector2(0,0)
-        wandrStr = .12
+        wandrStr = .13
         maxSpeed = 12
-        steerStr = 3
+        steerStr = 3  # 3 or 4
 
         if self.mode == 0 and self.pos.distance_to(self.nest) > 42:
             self.mode = 1
 
         psSize = (int(curW/PRATIO), int(curH/PRATIO))
         scaledown_pos = (int((self.pos.x/curW)*psSize[0]), int((self.pos.y/curH)*psSize[1]))
-        color_rgb = (0,0,240)
+        color_rgb = (0,0,200)
         # check if current pos diff from last pos
         if scaledown_pos != self.last_sdp and scaledown_pos[0] in range(0,psSize[0]) and scaledown_pos[1] in range(0,psSize[1]):
-            self.phero.input2grid(scaledown_pos, color_rgb)
+            #self.phero.input2grid(scaledown_pos, color_rgb)
+            self.phero.img_array[scaledown_pos] += color_rgb
             self.last_sdp = scaledown_pos
 
         #mid_sensr = vec2round(self.pos + pg.Vector2(20, 0).rotate(self.ang))#.normalize() # directional vec forward
         mid_sensL = Vec2.vint(self.pos + pg.Vector2(21, -3).rotate(self.ang))
         mid_sensR = Vec2.vint(self.pos + pg.Vector2(21, 3).rotate(self.ang))
-        left_sensr1 = Vec2.vint(self.pos + pg.Vector2(18, -14).rotate(self.ang))
-        left_sensr2 = Vec2.vint(self.pos + pg.Vector2(16, -21).rotate(self.ang))
-        right_sensr1 = Vec2.vint(self.pos + pg.Vector2(18, 14).rotate(self.ang))
-        right_sensr2 = Vec2.vint(self.pos + pg.Vector2(16, 21).rotate(self.ang))
+        left_sens1 = Vec2.vint(self.pos + pg.Vector2(18, -14).rotate(self.ang))
+        left_sens2 = Vec2.vint(self.pos + pg.Vector2(16, -21).rotate(self.ang))
+        right_sens1 = Vec2.vint(self.pos + pg.Vector2(18, 14).rotate(self.ang))
+        right_sens2 = Vec2.vint(self.pos + pg.Vector2(16, 21).rotate(self.ang))
         # either mid sensor needs to be a bit in front, or side sensors need to be more back..
 
         if self.drawSurf.get_rect().collidepoint(mid_sensL) and self.drawSurf.get_rect().collidepoint(mid_sensR):
-            mid_sL = self.drawSurf.get_at(mid_sensL)[:3]
-            mid_sR = self.drawSurf.get_at(mid_sensR)[:3]
-            mid_result = (max(mid_sL[0], mid_sR[0]), max(mid_sL[1], mid_sR[1]), max(mid_sL[2], mid_sR[2]))
-        if self.drawSurf.get_rect().collidepoint(left_sensr1) and self.drawSurf.get_rect().collidepoint(left_sensr2):
-            left_s1 = self.drawSurf.get_at(left_sensr1)[:3]
-            left_s2 = self.drawSurf.get_at(left_sensr2)[:3]
-            left_result = (max(left_s1[0], left_s2[0]), max(left_s1[1], left_s2[1]), max(left_s1[2], left_s2[2]))
-        if self.drawSurf.get_rect().collidepoint(right_sensr1) and self.drawSurf.get_rect().collidepoint(right_sensr2):
-            right_s1 = self.drawSurf.get_at(right_sensr1)[:3]
-            right_s2 = self.drawSurf.get_at(right_sensr2)[:3]
-            right_result = (max(right_s1[0], right_s2[0]), max(right_s1[1], right_s2[1]), max(right_s1[2], right_s2[2]))
+            #mid_L_sdpos = (int((mid_sensL[0]/curW)*psSize[0]), int((mid_sensL[1]/curH)*psSize[1]))
+            #mid_L_result = self.phero.img_array[mid_L_sdpos]
+            #mid_R_sdpos = (int((mid_sensR[0]/curW)*psSize[0]), int((mid_sensR[1]/curH)*psSize[1]))
+            #mid_R_result = self.phero.img_array[mid_R_sdpos]
+            #mid_A_result = (max(mid_L_result[0], mid_R_result[0]), max(mid_L_result[1], mid_R_result[1]), max(mid_L_result[2], mid_R_result[2]))
+            mid_A_result = self.sensArray(mid_sensL, mid_sensR)
+            #mid_gaL = self.drawSurf.get_at(mid_sensL)[:3]
+            #mid_gaR = self.drawSurf.get_at(mid_sensR)[:3]
+            #mid_result = (max(mid_gaL[0], mid_gaR[0]), max(mid_gaL[1], mid_gaR[1]), max(mid_gaL[2], mid_gaR[2]))
+            mid_result = self.sensGA(mid_sensL, mid_sensR)
+        if self.drawSurf.get_rect().collidepoint(left_sens1) and self.drawSurf.get_rect().collidepoint(left_sens2):
+            left_A_result = self.sensArray(left_sens1, left_sens2)
+            #left_ga1 = self.drawSurf.get_at(left_sens1)[:3]
+            #left_ga2 = self.drawSurf.get_at(left_sens2)[:3]
+            #left_result = (max(left_ga1[0], left_ga2[0]), max(left_ga1[1], left_ga2[1]), max(left_ga1[2], left_ga2[2]))
+            left_result = self.sensGA(left_sens1, left_sens2)
+        if self.drawSurf.get_rect().collidepoint(right_sens1) and self.drawSurf.get_rect().collidepoint(right_sens2):
+            right_A_result = self.sensArray(right_sens1, right_sens2)
+            #right_ga1 = self.drawSurf.get_at(right_sens1)[:3]
+            #right_ga2 = self.drawSurf.get_at(right_sens2)[:3]
+            #right_result = (max(right_ga1[0], right_ga2[0]), max(right_ga1[1], right_ga2[1]), max(right_ga1[2], right_ga2[2]))
+            right_result = self.sensGA(right_sens1, right_sens2)
         # INSTEAD OF get_at, try checking Array color at sensor spot equivalent scaledown locations
 
 
-        if mid_result[2] > max(left_result[2], right_result[2]) and mid_result[:2] == (0,0):
+        if mid_A_result[2] > max(left_A_result[2], right_A_result[2]) and mid_A_result[:2] == (0,0):
             self.desireDir += pg.Vector2(1,0).rotate(self.ang).normalize() # might not need +=
-            wandrStr = 0
-        elif left_result[2] > right_result[2] and left_result[:2] == (0,0):
+            wandrStr = .01
+        elif left_A_result[2] > right_A_result[2] and left_A_result[:2] == (0,0):
             self.desireDir += pg.Vector2(1,-2).rotate(self.ang).normalize() #left (0,-1)
-            wandrStr = 0
-        elif right_result[2] > left_result[2] and right_result[:2] == (0,0):
+            wandrStr = .01
+        elif right_A_result[2] > left_A_result[2] and right_A_result[:2] == (0,0):
             self.desireDir += pg.Vector2(1,2).rotate(self.ang).normalize() #right (0, 1)
-            wandrStr = 0
+            wandrStr = .01
 
         '''
         if self.mode == 1:
@@ -103,26 +116,26 @@ class Ant(pg.sprite.Sprite):
                 self.mode = 2
             elif mid_result[1] > max(left_result[1], right_result[1]) and (mid_result[0],mid_result[2]) == (0,0):
                 self.desireDir += pg.Vector2(1,0).rotate(self.ang).normalize()
-                wandrStr = 0
+                wandrStr = .1
             elif left_result[1] > right_result[1] and (left_result[0],left_result[2]) == (0,0):
                 self.desireDir += pg.Vector2(1,-2).rotate(self.ang).normalize() #left (0,-1)
-                wandrStr = 0
+                wandrStr = .1
             elif right_result[1] > left_result[1] and (right_result[0],right_result[2]) == (0,0):
                 self.desireDir += pg.Vector2(1,2).rotate(self.ang).normalize() #right (0, 1)
-                wandrStr = 0
+                wandrStr = .1
         elif self.mode == 2:
             if self.pos.distance_to(self.nest) < 32:
                 self.desireDir += pg.Vector2(-1,0).rotate(self.ang).normalize()
                 self.mode = 1
             elif mid_result[2] > max(left_result[2], right_result[2]) and mid_result[:2] == (0,0):
                 self.desireDir += pg.Vector2(1,0).rotate(self.ang).normalize()
-                wandrStr = 0
+                wandrStr = .1
             elif left_result[2] > right_result[2] and left_result[:2] == (0,0):
                 self.desireDir += pg.Vector2(1,-2).rotate(self.ang).normalize() #left (0,-1)
-                wandrStr = 0
+                wandrStr = .1
             elif right_result[2] > left_result[2] and right_result[:2] == (0,0):
                 self.desireDir += pg.Vector2(1,2).rotate(self.ang).normalize() #right (0, 1)
-                wandrStr = 0
+                wandrStr = .1
             else:
                 self.desireDir += pg.Vector2(self.nest - self.pos).normalize() * .1
                 wandrStr = .1   #pg.Vector2(self.desireDir + (1,0)).rotate(pg.math.Vector2.as_polar(self.nest - self.pos)[1])
@@ -132,13 +145,13 @@ class Ant(pg.sprite.Sprite):
                 self.mode = 2
             elif mid_result[1] > max(left_result[1], right_result[1]) and (mid_result[0],mid_result[2]) == (0,0):
                 self.desireDir += pg.Vector2(1,0).rotate(self.ang).normalize()
-                wandrStr = 0
+                wandrStr = .1
             elif left_result[1] > right_result[1] and (left_result[0],left_result[2]) == (0,0):
                 self.desireDir += pg.Vector2(1,-2).rotate(self.ang).normalize() #left (0,-1)
-                wandrStr = 0
+                wandrStr = .1
             elif right_result[1] > left_result[1] and (right_result[0],right_result[2]) == (0,0):
                 self.desireDir += pg.Vector2(1,2).rotate(self.ang).normalize() #right (0, 1)
-                wandrStr = 0
+                wandrStr = .1
             if self.pos.distance_to(self.nest) < 32:
                 self.desireDir += pg.Vector2(-1,0).rotate(self.ang).normalize()
                 self.mode = 1
@@ -162,17 +175,17 @@ class Ant(pg.sprite.Sprite):
             if self.mode == 1 : self.mode = 3
 
         # Avoid edges
-        if not self.drawSurf.get_rect().collidepoint(left_sensr2) and self.drawSurf.get_rect().collidepoint(right_sensr2):
-            self.desireDir += pg.Vector2(0,1).rotate(self.ang)
-            wandrStr = 0
+        if not self.drawSurf.get_rect().collidepoint(left_sens2) and self.drawSurf.get_rect().collidepoint(right_sens2):
+            self.desireDir += pg.Vector2(0,1).rotate(self.ang) #.normalize()
+            wandrStr = .01
             steerStr = 4
-        elif not self.drawSurf.get_rect().collidepoint(right_sensr2) and self.drawSurf.get_rect().collidepoint(left_sensr2):
-            self.desireDir += pg.Vector2(0,-1).rotate(self.ang)
-            wandrStr = 0
+        elif not self.drawSurf.get_rect().collidepoint(right_sens2) and self.drawSurf.get_rect().collidepoint(left_sens2):
+            self.desireDir += pg.Vector2(0,-1).rotate(self.ang) #.normalize()
+            wandrStr = .01
             steerStr = 4
         elif not self.drawSurf.get_rect().collidepoint(Vec2.vint(self.pos + pg.Vector2(21, 0).rotate(self.ang))):
-            self.desireDir += pg.Vector2(-1,0).rotate(self.ang)
-            wandrStr = 0
+            self.desireDir += pg.Vector2(-1,0).rotate(self.ang) #.normalize()
+            wandrStr = .01
             steerStr = 5
         #elif not self.drawSurf.get_rect().collidepoint(self.pos): #self.drawSurf.get_rect().contains(self.rect):
         #    self.desireDir = pg.Vector2((curW/2 - self.rect.centerx, curH/2 - self.rect.centery)).normalize()
@@ -200,19 +213,33 @@ class Ant(pg.sprite.Sprite):
         # actually update position
         self.rect.center = self.pos
 
+    def sensArray(self, sens_pos1, sens_pos2):
+        curW, curH = self.drawSurf.get_size()
+        psSize = (int(curW/PRATIO), int(curH/PRATIO))
+        sdpos1 = (int((sens_pos1[0]/curW)*psSize[0]), int((sens_pos1[1]/curH)*psSize[1]))
+        result1 = self.phero.img_array[sdpos1]
+        sdpos2 = (int((sens_pos2[0]/curW)*psSize[0]), int((sens_pos2[1]/curH)*psSize[1]))
+        result2 = self.phero.img_array[sdpos2]
+        return (max(result1[0], result2[0]), max(result1[1], result2[1]), max(result1[2], result2[2]))
+
+    def sensGA(self, ga_pos1, ga_pos2):
+        ga_r1 = self.drawSurf.get_at(ga_pos1)[:3]
+        ga_r2 = self.drawSurf.get_at(ga_pos2)[:3]
+        return (max(ga_r1[0], ga_r2[0]), max(ga_r1[1], ga_r2[1]), max(ga_r1[2], ga_r2[2]))
+
 class PheroGrid():
     def __init__(self, bigSize):
         self.surfSize = (bigSize[0]/PRATIO, bigSize[1]/PRATIO)
         self.image = pg.Surface(self.surfSize).convert()
         self.img_array = np.array(pg.surfarray.array3d(self.image)).astype(np.float64)
-    def input2grid(self, scaled_pos, color_rgb):
-        self.img_array[scaled_pos] += color_rgb
-        self.img_array[self.img_array > 255] = 255
     def update(self, dt):
-        self.img_array[self.img_array > 0] -= 1 * (60/FPS) * ((dt/10) * FPS)
+        self.img_array[self.img_array > 0] -= .9 * (60/FPS) * ((dt/10) * FPS)
         self.img_array[self.img_array < 1] = 0  # ensure no leftover floats <1
+        self.img_array[self.img_array > 255] = 255  # ensures nothing over 255
         pg.surfarray.blit_array(self.image, self.img_array)
         return self.image
+#    def input2grid(self, scaled_pos, color_rgb):
+#        self.img_array[scaled_pos] += color_rgb
 
 class Food(pg.sprite.Sprite):
     def __init__(self, pos):
@@ -267,10 +294,10 @@ def main():
             if e.type == pg.QUIT or e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
                 return
             elif e.type == pg.MOUSEBUTTONDOWN:
-                if e.button == 1:
+                mousepos = pg.mouse.get_pos()
+                if e.button == 1: # and pg.Vector2(mousepos).distance_to(nest) > 242:
                     foodBits = 200
                     fRadius = 50
-                    mousepos = pg.mouse.get_pos()
                     for i in range(0, foodBits):
                         dist = pow(i / (foodBits - 1.0), 0.5) * fRadius
                         angle = 2 * pi * 0.618033 * i
@@ -279,9 +306,8 @@ def main():
                         foods.add(Food((fx,fy)))
                     foodList.extend(foods.sprites())
                 if e.button == 3:
-                    mousepos = pg.mouse.get_pos()
                     for fbit in foodList:
-                        if pg.Vector2(fbit.rect.center).distance_to(mousepos) < fRadius:
+                        if pg.Vector2(mousepos).distance_to(fbit.rect.center) < fRadius:
                             fbit.pickup()
                     foodList = foods.sprites()
 
